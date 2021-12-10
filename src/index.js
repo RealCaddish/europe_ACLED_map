b8bb2a644a2b093f3e4995fe4f0cdbead430a99b
Nathaniel <nathaniel.deaton@gmail.com>
2021-12-10 11:47:22 -0500

    adding json data, ukraine,russia,belarus csv, webpack configs/packages.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import data from './data/cleaned_conflicts.csv';
 
 function component() {
   const element = document.createElement('h1');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello there,', 'webpack'], ' ');
 
   return element;
+
 }
 
+console.log(data)
+
 document.body.appendChild(component());
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -11,6 +22,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
       },
     ],
   },
